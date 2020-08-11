232b7dbe90b07a7420e1ff150a972c084f463131
Michael McCann <michael@mccanns.org>
2020-08-10 20:53:46 -0700

    Setup ping endpoint in file service

--- file-service/src/index.ts
+++ file-service/src/index.ts
@@ -1,3 +1,13 @@
-import fs from "fs"
+import express from "express"
 
-console.log("derp", typeof fs)
+const portNumber = process.env.NODE_PORT || 3000
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("File service response")
+})
+
+app.listen(portNumber, () => {
+    console.log(`File service is now listening on port ${portNumber}`)
+})
